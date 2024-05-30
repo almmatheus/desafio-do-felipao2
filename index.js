39bed6ad24e3c2775d572e9454704504ed1dadc1
almmatheus <mathalmeida197@gmail.com>
2024-05-30 14:41:04 -0300

    adição do laço do while, permite que o usuário faça os cálculos de ranking

--- index.js
+++ index.js
@@ -16,23 +16,23 @@ function calculadoraDeRanking(vitorias, derrotas, nivel){
         nivel = 'Diamante'
     } else if (s >= 91 && s <= 100){
         nivel = 'Lendário'
-    } else if (s >= 101) {
+    } else {
         nivel = 'Imortal'
     }
 
     console.log(`O herói tem saldo de ${s} vitórias e está no nível ${nivel}`)
 }
 
-let saldoVitorias = prompt('Saldo de vitorias do herói: ')
-let saldoDerrotas = prompt('Saldo de derrotas do herói: ')
+let resp
+let saldoVitorias
+let saldoDerrotas
 let ranking
 
-calculadoraDeRanking(saldoVitorias, saldoDerrotas, ranking)
-
-// Se vitórias for menor do que 10 = Ferro
-// Se vitórias for entre 11 e 20 = Bronze
-// Se vitórias for entre 21 e 50 = Prata
-// Se vitórias for entre 51 e 80 = Ouro
-// Se vitórias for entre 81 e 90 = Diamante
-// Se vitórias for entre 91 e 100= Lendário
-// Se vitórias for maior ou igual a 101 = Imortal
+do{
+    
+    saldoVitorias = prompt('Saldo de vitorias do herói: ')
+    saldoDerrotas = prompt('Saldo de derrotas do herói: ')
+    calculadoraDeRanking(saldoVitorias, saldoDerrotas, ranking)
+    resp = prompt('Fazer novo cálculo de ranking? [S/N] ')
+    
+} while (resp === 'S' || resp === 's')
